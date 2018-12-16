bdd33181d37ba372ee94e0ab79e983958e16f960
silnichii <silnichii@gmail.com>
2018-12-16 02:07:26 +0100

    grid extend buttons, zindex fix

--- js/main.js
+++ js/main.js
@@ -1,15 +1,44 @@
 document.addEventListener("DOMContentLoaded", ()=>{
 
-    let cells = document.querySelectorAll(".cell");
+    let gridSize = {
+        rows: 10,
+        colums: 10
+    }
+    
+    let grid = document.querySelector("#grid");
+    let cells
     let itemsList = document.querySelector(".items-list");
     let itemsListWrap = document.querySelector(".items-list-wrap");
     let items = itemsList.querySelectorAll(".item");
     let itemsHistory = document.querySelector(".items-history");
     let closeBtn = document.querySelector(".close-list");
+    let addGridRowBottom = document.querySelector(".add-row-bottom");
+    let addGridRowTop = document.querySelector(".add-row-top");
+    let addGridColumnLeft = document.querySelector(".add-column-left");
+    let addGridColumnRight = document.querySelector(".add-column-right");
 
     let currentCell
 
     itemsArr = []
+
+    gridRender(gridSize.rows, gridSize.colums);
+
+
+    function gridRender(rows, colums){
+        while (grid.children[4]){
+            grid.removeChild(grid.children[2]);
+        }
+        grid.style.gridTemplateColumns = `repeat(${colums}, 50px)`;
+        grid.style.gridTemplateRows = `repeat(${rows}, 50px)`;
+
+        let cell = document.createElement("div");
+        cell.classList = "cell";
+        for (let i = 0; i < rows * colums; i++ ){
+            grid.appendChild(cell.cloneNode(true));
+        }
+        cells = grid.querySelectorAll(".cell");
+        cellsInit()
+    }
 
     function idGenerator() {
         var S4 = function() {
@@ -130,39 +159,62 @@ document.addEventListener("DOMContentLoaded", ()=>{
         });
     }
 
-    cells.forEach((cell, index) => {
-        cell.dataset.id = index+1;
-
-        cell.addEventListener("click", function _listener(){
-            if(!itemsListWrap.classList.contains("items-list-active")){
-                itemsListWrap.classList.add("items-list-active");
-                closeBtn.classList.add("close-list-active");
-                
-            }
-            cells.forEach(i => {i.classList.remove("cell-active")})
-            cell.classList.add("cell-active");
-
-            new Promise(function(resolve, reject) {
-
-                resolve(cell)
-    
-            }).then(function(cell){
-            
-                items.forEach(item => {
-                    item.removeEventListener("click", clone)
-                    item.removeEventListener("click", renderToGrid)
-                    item.removeEventListener("click", renderHistory)
-                    item.addEventListener("click", clone)
-                    item.addEventListener("click", renderToGrid)
-                    item.addEventListener("click", renderHistory)
-                })
-                currentCell = {
-                    "id": cell.dataset.id,
-                    "data": cell.getBoundingClientRect()
+    function cellsInit(){
+        cells.forEach((cell, index) => {
+            if(!cell.dataset.id){
+                cell.dataset.id = idGenerator();
+            }    
+            cell.addEventListener("click", function _listener(){
+                if(!itemsListWrap.classList.contains("items-list-active")){
+                    itemsListWrap.classList.add("items-list-active");
+                    closeBtn.classList.add("close-list-active");
                 }
-            })
-        });
-    });
+    
+                cells.forEach(i => {i.classList.remove("cell-active")})
+                cell.classList.add("cell-active");
+    
+                new Promise(function(resolve, reject) {
+    
+                    resolve(cell)
+        
+                }).then(function(cell){
+                
+                    items.forEach(item => {
+                        item.removeEventListener("click", clone)
+                        item.removeEventListener("click", renderToGrid)
+                        item.removeEventListener("click", renderHistory)
+                        item.addEventListener("click", clone)
+                        item.addEventListener("click", renderToGrid)
+                        item.addEventListener("click", renderHistory)
+                    })
+                    currentCell = {
+                        "id": cell.dataset.id,
+                        "data": cell.getBoundingClientRect()
+                    }
+                })
+            });
+        });    
+    }
+
+    function setZindex(){
+        cells.forEach(cell=>{
+            if (cell.children[0]){
+                itemsArr.forEach(i=>{
+                    if(i.id == cell.children[0].dataset.id){
+                        if (cell.children[0].classList.contains("road")){
+                            i.zIndex = 1
+                        }else if(cell.children[0].classList.contains("l")) {
+                            i.zIndex = parseInt(cell.getBoundingClientRect().top)+19
+                        }else if(cell.children[0].classList.contains("m")) {
+                            i.zIndex = parseInt(cell.getBoundingClientRect().top)+10
+                        }else {
+                            i.zIndex = parseInt(cell.getBoundingClientRect().top)
+                        }        
+                    }
+                })
+            }
+        })
+    }
 
 
 
@@ -170,8 +222,72 @@ document.addEventListener("DOMContentLoaded", ()=>{
     closeBtn.addEventListener("click", function(){
         itemsListWrap.classList.remove("items-list-active")
         this.classList.remove("close-list-active")
-    })
-   
+    });
+
+    addGridRowBottom.addEventListener("click", ()=>{
+        gridSize.rows ++
+        for(let i=0; i<gridSize.colums; i++){
+            newCell = grid.lastChild.cloneNode(true);
+            newCell.dataset.id = idGenerator();
+            grid.insertBefore(newCell, grid.children[(gridSize.rows * gridSize.colums) - gridSize.rows + 5]);
+        }
+        grid.style.gridTemplateRows = `repeat(${gridSize.rows}, 50px)`;
+        cells = grid.querySelectorAll(".cell");
+        cellsInit();
+        setZindex();
+        renderHistory();
+        renderToGrid();
+
+    });
+    addGridRowTop.addEventListener("click", ()=>{
+        gridSize.rows ++
+        for(let i=0; i<gridSize.colums; i++){
+            newCell = grid.lastChild.cloneNode(true);
+            newCell.dataset.id = idGenerator();
+            grid.insertBefore(newCell, grid.children[4]);
+        }
+        grid.style.gridTemplateRows = `repeat(${gridSize.rows}, 50px)`;
+        cells = grid.querySelectorAll(".cell");
+        cellsInit();
+        setZindex();
+        renderHistory();
+        renderToGrid();
+    });
+
+    addGridColumnLeft.addEventListener("click", ()=>{
+        gridSize.colums ++
+        for(let i=0, j=4; i<gridSize.rows; i++, j+=gridSize.colums){
+            newCell = grid.lastChild.cloneNode(true);
+            newCell.dataset.id = idGenerator();
+            grid.insertBefore(newCell, grid.children[j]);
+        }
+        grid.style.gridTemplateColumns = `repeat(${gridSize.colums}, 50px)`;
+        cells = grid.querySelectorAll(".cell");
+        cellsInit();
+        setZindex();
+        renderHistory();
+        renderToGrid();
+    });
+
+    addGridColumnRight.addEventListener("click", ()=>{
+        gridSize.colums ++
+        for(let i=0, j=4+gridSize.colums-1; i<gridSize.rows; i++, j+=gridSize.colums){
+            newCell = grid.lastChild.cloneNode(true);
+            newCell.dataset.id = idGenerator();
+            grid.insertBefore(newCell, grid.children[j]);
+        }
+        grid.style.gridTemplateColumns = `repeat(${gridSize.colums}, 50px)`;
+        cells = grid.querySelectorAll(".cell");
+        cellsInit();
+        setZindex();
+        renderHistory();
+        renderToGrid();
+    });
+
+
+
+    
+// dom load
 })
 
 
